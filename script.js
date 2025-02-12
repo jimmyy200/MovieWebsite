81b856393387eec8c3a3d6caef0421ff70ce5826
jimmyy200 <jimmy322@my.yorku.ca>
2025-02-12 12:27:40 -0500

    Created javascript file

---
+++ script.js
@@ -0,0 +1,26 @@
+const api_key = "api_key=1201a6dde19d9c28d35c8366a5542d19";
+const base_url = "https://api.themoviedb.org/3";
+const api_url = base_url + "/discover/movie?sort_by=popularity.desc&" + api_key;
+const img_url = "https://image.tmdb.org/t/p/w500";
+const searchURL = base_url + "/search/movie?" + api_key;
+
+fetchMovies(api_url);
+
+function fetchMovies(url){
+    fetch(url).then(res => res.json()).then(data =>{
+        console.log(data);
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data){
+    main.innerHTML= ""
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = "<img src='' alt='img'>"
+        main.appendChild(movieEl);
+    })
+}
